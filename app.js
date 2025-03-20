16807faf190e96bc71317d58dcd354e578a5a483
ishaqfarooq99 <ishaqfarooq000@gmail.com>
2025-03-20 11:55:24 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-function btnClick(a){
-    var getInp = document.getElementById(`int`);
-    getInp.value += a;
+
+
+
+var miliseconds = 0;
+var seconds = 0;
+var minutes = 0;
+
+ var getmsec = document.getElementById("msec")
+ var getsec = document.getElementById("sec")
+ var getmint = document.getElementById("mint")
+
+ var interval;
+
+ 
+ function start(){
+   interval = setInterval(function(){
+        miliseconds++
+        getmsec.innerHTML = miliseconds
+        if(miliseconds >= 100){
+           seconds++
+           getsec.innerHTML = seconds
+           miliseconds = 0
+        }
+        else if(seconds >= 59){
+           minutes++
+           getmint.innerHTML = minutes
+           seconds = 0
+        }
+   document.getElementById('button').disabled = true
+        
+    }, 10)
+   
+ }
+
+ function stop(){
+    clearInterval(interval)
+    document.getElementById('button').disabled = false
 }
 
-function eq(){
-    var getInp = document.getElementById(`int`);
-    getInp.value = eval(getInp.value);
+function reset(){
+    miliseconds = 0
+    seconds = 0
+    minutes = 0
+    getmsec.innerHTML = miliseconds
+    getsec.innerHTML = seconds
+    getmint.innerHTML = minutes
 }
 
-function clrAll(){
-    var getInp = document.getElementById(`int`);
-    getInp.value = ``;
-}
 
-function clr(){
-    var getInp = document.getElementById(`int`);
-    getInp.value = getInp.value.slice(0,-1);
-}
+
+
+
+
+
+
+
+
+
